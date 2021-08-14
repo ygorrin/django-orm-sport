122519dec62d9b04cbdfa4a00a66e7cfb156b69a
Yamy <yamilagorrin@gmail.com>
2021-08-14 10:18:49 -0400

    Primer ejercicio

--- leagues/views.py
+++ leagues/views.py
@@ -8,6 +8,24 @@ def index(request):
 		"leagues": League.objects.all(),
 		"teams": Team.objects.all(),
 		"players": Player.objects.all(),
+		"liga_mujeres": League.objects.filter(name__contains = "womens"),
+		"liga_hockey": League.objects.filter(sport__contains = "hockey"),
+		"liga_no_football": League.objects.exclude(sport__contains = "football"),
+		"liga_conferencia": League.objects.filter(name__contains = "conference"),
+		"liga_atlantica": League.objects.filter(name__contains = "Atlantic"),
+		"equipo_dallas": Team.objects.filter(location__contains = "Dallas"),
+		"equipo_raptors": Team.objects.filter(team_name__contains = "Raptors"),
+		"equipo_city": Team.objects.filter(location__contains = "City"),
+		"equipo_t": Team.objects.filter(team_name__startswith = "t"),
+		"equipo_orden_ubicacion": Team.objects.all().order_by('location'),
+		"equipo_orden_nombre": Team.objects.all().order_by('-team_name'),
+		"jugador_cooper": Player.objects.filter(last_name__contains = "cooper"),
+		"jugador_joshua": Player.objects.filter(first_name__contains = "joshua"),
+		"jugador_cooper_no_joshua": Player.objects.filter(last_name__contains = "cooper").exclude(first_name__contains = "joshua"),
+		"jugador_alexander_o_wyatt": Player.objects.filter(first_name__contains = ["Alexander", "wyatt"]),
+
+
+
 	}
 	return render(request, "leagues/index.html", context)
 
